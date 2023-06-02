c22f5bf938c3a8624e5acce52c0180f0891fdbc5
rongzhus <shangrz@cn.ibm.com>
2023-06-02 15:16:10 +0800

    refine bundle-example2

--- examples/bundle-example2/index.js
+++ examples/bundle-example2/index.js
@@ -1,12 +1,5 @@
-const assert = require('assert');
-
-const requestURL = 'https://httpbin.org/get';
-$http.get(requestURL, {
-  strictSSL: false, // false for self signed certificate
-}, function(err, response, body) {
-  if (err) throw err;
-  assert.equal(response.statusCode, 200, "Expected a 200 OK response");
-  console.info('request url: %s, statusCode: %d', requestURL, response.statusCode);
-});
+const sendHTTP = require('./sendRequest');
 
 console.debug("This is an example of bundle script test");
+
+sendHTTP();
