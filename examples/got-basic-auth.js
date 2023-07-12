820a8bd2bb3ddd949bb4ae9e8e319ad55ec07fd6
rongzhus <shangrz@cn.ibm.com>
2023-07-12 21:40:08 +0800

    Refine credentials example (#4)
    * 1.0.7
    
    * refine creds
    
    * refine example
    
    * resolve comments
    
    * update readme

---
+++ examples/got-basic-auth.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+(async function(){
+    const auth = Buffer.from(`${$secure.username}:${$secure.password}`).toString('base64');
+    console.debug('auth is', auth);
+    let gotResponse = await $got.get('https://httpbin.org/get', {
+      https:{ rejectUnauthorized: false },
+      headers: {
+        Authorization: `Basic ${auth}`
+      }}
+    );
+
+    assert.ok(gotResponse.statusCode == 200, "GET status is " + gotResponse.statusCode + ", it should be 200");
+})();
--- examples/got-get.js
+++ examples/got-get.js
@@ -11,4 +11,10 @@ var assert = require('assert');
   let response = await $got.get(options);
   assert.equal(response.statusCode, 200, 'Expected a 200 OK response');
   console.log('Request URL %s, statusCode: %d', response.url, response.statusCode);
-})();
+
+  // use credentials
+  // console.log('$secure.username', $secure.username)
+  // console.log('$secure.password', $secure.password)
+})()
+// catch error
+// .catch(error=>console.error('error:', error))
