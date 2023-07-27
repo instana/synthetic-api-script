65e2f08e06be18d3525641a06b4233400db3db7d
rongzhus <shangrz@cn.ibm.com>
2023-07-27 17:02:31 +0800

    update example

--- examples/ssl-checker.js
+++ examples/ssl-checker.js
@@ -2,13 +2,18 @@ const sslChecker = require('ssl-checker');
 const assert = require('assert');
 
 const hostname = 'ibm.com';
+const remainDays = 90;
 
 const getSslDetails = async(hostname) => {
   const result = await sslChecker(hostname);
-  assert.equal(result.valid, true, 'certificate of ibm should be valid');
   console.log(`certificate is valid: ${result.valid}`);
   console.log(`certificate expires on: ${result.validTo}`);
   console.log(`certificate days remaining: ${result.daysRemaining}`);
+
+  assert.equal(result.valid, true, 'certificate of ibm should be valid');
+  // this script will fail if the certificate remaining days less than 90 days by default
+  // modify variable remainDays to any value as you need
+  assert.equal(result.daysRemaining >= remainDays, true, `certificate validated remain days is less than ${remainDays} days`);
 };
 
 getSslDetails(hostname);
